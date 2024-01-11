4c60a3311c111251939309c95cd4684673335d12
patilrushikesh247 <58651016+patilrushikesh247@users.noreply.github.com>
2024-01-11 09:06:31 +0000

    conversions operatoin

---
+++ .devcontainer/javascript/conversionorOperation.js
@@ -0,0 +1,30 @@
+let score ='dd'
+
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let convertIntoNo=Number(score)
+// console.log(typeof convertIntoNo);
+// console.log(convertIntoNo)
+
+// notes
+//"33"=>33
+//"22432dd" => NaN
+//true =>1
+//false=>0
+
+let isLoggedIn='33'
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// notes
+//1=>true
+//0=>false
+//""=>false
+//"hrushikesh"=>true
+
+let someNumber =33
+
+let convertString= String(someNumber)
+console.log( typeof convertString )
