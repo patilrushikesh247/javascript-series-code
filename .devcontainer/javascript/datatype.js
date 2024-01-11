7a1415fa1d2631fa195b61e557920a79feaefe70
patilrushikesh247 <58651016+patilrushikesh247@users.noreply.github.com>
2024-01-11 08:15:40 +0000

    data type and variable

---
+++ .devcontainer/javascript/datatype.js
@@ -0,0 +1,24 @@
+"use strict"//  it use all new js code version
+alert('hello') //we are useing node js not browser
+// console.log('sdf') console.log('done ')  //do not use this type of code because its bad practise for code readablity
+
+
+// * premetive data types
+//number => 2 to power 53
+// bigint
+// string=>""
+//boolean=> true/false
+//null=> standalone value representation of empty value
+//undefined =>  when value not declare
+//symbol => for uniqueness we use symbol
+
+
+
+// *not premetive data type
+
+console.log(typeof age)
+
+ console.log(typeof null)
+ //output is object
+ console.log(typeof undefined)
+ //output is undefiend
---
+++ .devcontainer/javascript/variable.js
@@ -0,0 +1,10 @@
+const accountID=13132
+let emailID= "patilrushikesh@gmail.com"
+var accountPassword="2342"
+accountCity="Pune"
+
+// accountID=2 not allowed to change
+console.log(accountID);
+console.table([accountID,emailID,accountPassword,accountCity]);
+
+/* prefered not to use var beacuase of issue in block scope and functional scope*/
